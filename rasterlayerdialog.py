418d98357842ba8f1b11f06f032550fc47dfe74d
carolinux <karolina.alexiou@teralytics.ch>
2015-05-17 22:22:59 +0200

    Made raster handling more intuitive

--- rasterlayerdialog.py
+++ rasterlayerdialog.py
@@ -14,6 +14,9 @@ from logging import info, warn, error
 class RasterLayerDialog(AddLayerDialog):
     TIME_REGEX = "([^\d])*(\d[\d_:\\-\.]*\d)([^\d])*"
 
+    startChecked = False
+    endChecked = False
+
     def __init__(self, *args):
         super(RasterLayerDialog, self).__init__(*args)
     
@@ -22,6 +25,8 @@ class RasterLayerDialog(AddLayerDialog):
 
     def add_connections(self):
         super(RasterLayerDialog, self).add_connections()
+        self.dialog.checkBoxStart.setChecked(Qt.Checked if RasterLayerDialog.startChecked else Qt.Unchecked)
+        self.dialog.checkBoxEnd.setChecked(Qt.Checked if RasterLayerDialog.endChecked else Qt.Unchecked)
         self.dialog.spinBoxStart1.valueChanged.connect(self.refreshStart)
         self.dialog.spinBoxStart2.valueChanged.connect(self.refreshStart)
         self.dialog.spinBoxEnd1.valueChanged.connect(self.refreshEnd)
@@ -42,22 +47,29 @@ class RasterLayerDialog(AddLayerDialog):
 
     def guess_time_position_in_str(self, str_with_time):
         try:
-            m = re.match(TIME_REGEX, str_with_time)
+            m = re.match(self.TIME_REGEX, str_with_time)
             return m.start(2), m.end(2)
-        except:
+        except Exception, e:
+            info("Could not guess timestamp in raster filename. Cause {}".format(e))
             return (0,0)
 
     def refreshStart(self, ignore_val=0):
         if self.dialog.checkBoxStart.checkState() == Qt.Checked:
+            RasterLayerDialog.startChecked = True
             start = self.dialog.spinBoxStart1.value()
             end = self.dialog.spinBoxStart2.value()
             self.dialog.textStart.setText(self.getSelectedLayerName()[start:end])
+        else:
+            RasterLayerDialog.startChecked = False 
 
     def refreshEnd(self, ignore_val=0):
         if self.dialog.checkBoxEnd.checkState() == Qt.Checked:
+            RasterLayerDialog.endChecked = True
             start = self.dialog.spinBoxEnd1.value()
             end = self.dialog.spinBoxEnd2.value()
             self.dialog.textEnd.setText(self.getSelectedLayerName()[start:end])
+        else:
+            RasterLayerDialog.endChecked = False 
 
     def add_layer_attributes(self, idx):
         """get list layer attributes, fill the combo boxes"""
