56b0a6d35741b950f6bee850cafd2b7b042110d2
carolinux <karolina.alexiou@teralytics.ch>
2015-11-06 17:40:30 +0100

    Prepare 2.2

--- timemanager.py
+++ timemanager.py
@@ -33,7 +33,7 @@ class timemanager:
     name = "timemanager"
     longName = "TimeManager Plugin for QGIS >= 2.3"
     description = "Working with temporal vector data"
-    version = "Version 2.1.7"
+    version = "Version 2.2"
     qgisMinimumVersion = '2.3'
     author = "Anita Graser, Karolina Alexiou"
     pluginUrl = "https://github.com/anitagraser/TimeManager"
