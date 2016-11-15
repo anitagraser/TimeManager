1eee1613a44ba7dad48a06891711d6de903e1428
Richard Duivenvoorde <richard@duif.net>
2016-11-15 13:02:48 +0100

    fix for #14756, in which the feature count in the legend is not updated

--- timemanagercontrol.py
+++ timemanagercontrol.py
@@ -210,10 +210,22 @@ class TimeManagerControl(QObject):
             self.guiControl.repaintJoined()
             self.guiControl.repaintVectors()
             self.guiControl.refreshMapCanvas()
+            self.updateLegendCount()
         except Exception, e:
             error(e)
         finally:
             self.setPropagateGuiChanges(True)
+
+    def updateLegendCount(self):
+        """
+        This method is actually a hack/fix for http://hub.qgis.org/issues/14756.
+        Untill this is fixed via some signal/action in the legend(tree), below is needed.
+        :return:
+        """
+        root = QgsProject.instance().layerTreeRoot()
+        model = self.iface.layerTreeView().model()
+        for l in self.getTimeLayerManager().getManagedLayers():
+            model.refreshLayerLegend(root.findLayer(l.id()))
 
     def disableAnimationExport(self):
         """disable the animation export button"""
