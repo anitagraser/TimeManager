2de21504ab63624b574d1af638b050c7583c7fa4
Ariadni-Karolina Alexiou <carolinegr@gmail.com>
2018-01-30 15:06:23 +0100

    try not using coverage

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@ find -name '*.pyc' |xargs rm # assure that the build is clean
 if [ "$1" == "local" ]; then
    mv test/spatialite_experiments.py  test/test_with_spatialite.py # only run spatialite tests in local build, Travis has problems
 fi
-nosetests --with-coverage --cover-erase --cover-package=TimeManager $2
+nosetests --cover-package=TimeManager $2
 rc=$?
 if [ "$1" == "local" ]; then
     mv test/test_with_spatialite.py test/spatialite_experiments.py
