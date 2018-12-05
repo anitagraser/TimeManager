2ca8d99c8406a128fb2951448a441d06c06cd7d5
Karolina <karolina.alexiou@siroop.ch>
2018-12-05 21:29:25 +0000

    simplify run tests on travis

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 find -name '*.pyc' -delete
-if [ "$1" == "local" ]; then
-   mv test/spatialite_experiments.py  test/test_with_spatialite.py # only run spatialite tests in local build, Travis has problems
-fi
-nosetests --cover-package=TimeManager $2
+nosetests --cover-package=TimeManager 
 rc=$?
-if [ "$1" == "local" ]; then
-    mv test/test_with_spatialite.py test/spatialite_experiments.py
-fi
 exit $rc
